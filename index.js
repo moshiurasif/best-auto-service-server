d5030731b6e38fdb1bf513522c27374187193274
asif2212 <asifmd2212@gamil.com>
2021-04-20 23:56:30 +0600

    daone

--- index.js
+++ index.js
@@ -1,13 +1,117 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const ObjectId = require("mongodb").ObjectId;
+const fsExtra = require("fs-extra");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+// console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ongm4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const port = 8000;
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
-const port = 5000;
+app.use(express.json());
+app.use(express.static("customerImage"));
+app.use(fileUpload());
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
+
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const orderInfoCollection = client.db("gerezdb").collection("orderInformation");
+  const reviewInformationCollection = client
+    .db("gerezdb")
+    .collection("reviewInformation");
+    const servicesDataCollection = client
+      .db("gerezdb")
+      .collection("servicesData");
+  console.log("mongo connected");
+  // order
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    orderInfoCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    orderInfoCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  })
+
+  // add review
+  app.post("/addReview", (req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const companyName = req.body.companyName;
+    const customerOpinion = req.body.customerOpinion;
+    const newImg = file.data;
+    const encImg = newImg.toString("base64");
+
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, "base64"),
+    };
+
+    reviewInformationCollection
+      .insertOne({ name, companyName, image, customerOpinion })
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+  });
+
+  app.get("/reviews", (req, res) => {
+    reviewInformationCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  // add service
+  app.post("/addService", (req, res) => {
+    const file = req.files.file;
+    const serviceName = req.body.serviceName;
+    const description = req.body.description;
+    const price = req.body.price;
+    const newImg = file.data;
+    const encImg = newImg.toString("base64");
+
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, "base64"),
+    };
+
+    servicesDataCollection
+      .insertOne({ serviceName, description, image, price })
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+  });
+
+  app.get("/services", (req, res) => {
+    servicesDataCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/singleService", (req, res) => {
+    const id = req.body.id;
+    servicesDataCollection.find({ _id: ObjectId(id)}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
+
 
 app.listen(port);
